28737146324c0723fbf1d225b59776aad2c55fad
Erik Søe Sørensen <ess@trifork.com>
2012-10-31 16:55:21 +0100

    Chat client bugfix: Output shutdown didn't work.

--- src/chat_client.erl
+++ src/chat_client.erl
@@ -6,34 +6,38 @@
 %% chat_client:start(chatroom:local_chatroom()).
 
 start(Chatroom) ->
-    OutPid = proc_lib:spawn_link(fun() -> output_init(Chatroom) end),
-    input_loop(Chatroom, OutPid).
+    Ref = make_ref(),
+    OutPid = proc_lib:spawn_link(fun() -> output_init(Chatroom,Ref) end),
+    input_loop(Chatroom, OutPid, Ref).
 
-input_loop(Chatroom, OutPid) ->
+input_loop(Chatroom, OutPid, Ref) ->
     case io:get_line(standard_io, "> ") of
         eof -> ok;
         "/quit\n" ->
-            exit(OutPid, normal),
+            OutPid ! {stop,Ref},
             ok;
         Line0 ->
             Line = string:strip(Line0, right, $\n),
             (Line == []) orelse (Chatroom ! Line),
-            input_loop(Chatroom, OutPid)
+            input_loop(Chatroom, OutPid, Ref)
     end.
 
-output_init(Chatroom) ->
+output_init(Chatroom,Ref) ->
     Chatroom ! {join, self()},
-    output_loop().
+    output_loop(Ref).
 
-output_loop() ->
+output_loop(Ref) ->
     receive
+        {stop,Ref} ->
+            io:format("Left the chatroom.\n"),
+            ok;
         M ->
             case is_string(M) of
                 true -> io:format("~s\n", [M]);
                 false -> io:format("~p\n", [M])
-            end
-    end,
-    output_loop().
+            end,
+            output_loop(Ref)
+    end.
 
 is_string([]) -> true;
 is_string([H|T]) ->
